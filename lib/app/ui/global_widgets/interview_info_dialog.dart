eb3ea51b1777ea0eede05c18eb078a0c8a2141e1
Dasha Koshkina <koshkinadasha2002@gmail.com>
2021-11-25 01:32:03 +0200

    Added interview list

---
+++ lib/app/ui/global_widgets/interview_info_dialog.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import '../pages/home_page/tabs/calendar_tab.dart';
+
+class InterviewInfoDialog extends StatelessWidget {
+  const InterviewInfoDialog({Key? key, required this.meetings})
+      : super(key: key);
+
+  final List<Meeting> meetings;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        ListView.builder(
+          padding: const EdgeInsets.only(top: 20.0,bottom: 40.0),
+          shrinkWrap: true,
+          itemCount: meetings.length,
+          itemBuilder: (_, index) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Text(meetings[index].eventName),
+                  const SizedBox(height: 10.0),
+                  Text(meetings[index].from.toString()),
+                ],
+              ),
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
--- lib/app/ui/pages/home_page/tabs/calendar_tab.dart
+++ lib/app/ui/pages/home_page/tabs/calendar_tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:neophyte/app/ui/global_widgets/interview_info_dialog.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 import '../../../../utils/constants.dart';
@@ -14,6 +15,33 @@ class _CalendarTabState extends State<CalendarTab> {
     return Container(
       padding: const EdgeInsets.only(top: 20),
       child: SfCalendar(
+        onTap: (details) {
+          if (_getDataSource()
+              .where(
+                (e) =>
+                    e.from.year == details.date?.year &&
+                    e.from.month == details.date?.month &&
+                    e.from.day == details.date?.day,
+              )
+              .toList()
+              .isNotEmpty){
+            showBottomSheet(
+              context: context,
+              builder: (context) => InterviewInfoDialog(
+                meetings: _getDataSource()
+                    .where(
+                      (e) =>
+                          e.from.year == details.date?.year &&
+                          e.from.month == details.date?.month &&
+                          e.from.day == details.date?.day,
+                    )
+                    .toList(),
+              ),
+            );
+          }
+
+        },
+        dataSource: MeetingDataSource(_getDataSource()),
         view: CalendarView.month,
         showNavigationArrow: true,
         headerHeight: 80,
@@ -25,6 +53,85 @@ class _CalendarTabState extends State<CalendarTab> {
           shape: BoxShape.rectangle,
         ),
       ),
+    );
+  }
+
+  List<Meeting> _getDataSource() {
+    final List<Meeting> meetings = <Meeting>[
+      Meeting.test(2),
+      Meeting.test(5),
+      Meeting.test(6),
+      Meeting.test(6),
+      Meeting.test(6),
+      Meeting.test(7),
+      Meeting.test(8),
+      Meeting.test(9),
+      Meeting.test(12),
+    ];
+    return meetings;
+  }
+}
+
+class MeetingDataSource extends CalendarDataSource {
+  /// Creates a meeting data source, which used to set the appointment
+  /// collection to the calendar
+  MeetingDataSource(List<Meeting> source) {
+    appointments = source;
+  }
+
+  @override
+  DateTime getStartTime(int index) {
+    return _getMeetingData(index).from;
+  }
+
+  @override
+  DateTime getEndTime(int index) {
+    return _getMeetingData(index).to;
+  }
+
+  @override
+  String getSubject(int index) {
+    return _getMeetingData(index).eventName;
+  }
+
+  @override
+  Color getColor(int index) {
+    return _getMeetingData(index).background;
+  }
+
+  @override
+  bool isAllDay(int index) {
+    return _getMeetingData(index).isAllDay;
+  }
+
+  Meeting _getMeetingData(int index) {
+    final dynamic meeting = appointments![index];
+    late final Meeting meetingData;
+    if (meeting is Meeting) {
+      meetingData = meeting;
+    }
+
+    return meetingData;
+  }
+}
+
+class Meeting {
+  String eventName;
+  DateTime from;
+  DateTime to;
+  Color background;
+  bool isAllDay;
+
+  // ignore: sort_constructors_first
+  Meeting(this.eventName, this.from, this.to, this.background, this.isAllDay);
+
+  factory Meeting.test(int day) {
+    return Meeting(
+      'Daria Koshkina',
+      DateTime(2021, 11, day, 9, 0),
+      DateTime(2021, 11, day, 9, 15),
+      const Color(0xFF0F8644),
+      true,
     );
   }
 }
